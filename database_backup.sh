745b464c6dd55a5c628c66337a2c667f05ba2433
Damian Zaremba <damian@damianzaremba.co.uk>
2019-02-17 16:02:27 +0100

    Remove old user logic

--- database_backup.sh
+++ database_backup.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-if [ "$(whoami)" == "tools.cluebot" ];
-then
-    export DB_USER=s51109
-fi
-if [ "$(whoami)" == "tools.cluebotng" ];
-then
-    export DB_USER=s52585
-fi
-
-if [ -z "$DB_USER" ];
-then
-    echo "I don't know what to do!"
-    exit 1
-fi
-
 mkdir -p "$HOME/mysql_backups"
 filename=`date +"%d-%m-%Y_%H-%M-%S"`
 
 echo "Dumping the bot db"
-mysqldump  --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb $DB_USER"__cb" > "$HOME/mysql_backups/$filename-cb.sql"
+mysqldump  --defaults-file="${HOME}"/replica.my.cnf -h tools.labsdb s52585__cb > "$HOME/mysql_backups/$filename-cb.sql"
