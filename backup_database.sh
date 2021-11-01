eb7ddbc77b6d28f0b1d519116ce8b05d9ed02685
Damian Zaremba <damian@damianzaremba.co.uk>
2021-11-01 15:04:59 +0100

    gzip the resulting sql

--- backup_database.sh
+++ backup_database.sh
@@ -5,4 +5,4 @@ mkdir -p "$HOME/mysql_backups"
 mysqldump \
   --defaults-file="${HOME}"/replica.my.cnf \
   -h tools-db \
-  s52585__cb > "$HOME/mysql_backups/$(date +"%d-%m-%Y_%H-%M-%S")-cb.sql"
+  s52585__cb | gzip -9 > "$HOME/mysql_backups/$(date +"%d-%m-%Y_%H-%M-%S")-cb.sql.gz"
