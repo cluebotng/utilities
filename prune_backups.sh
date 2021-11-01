5c423b91f07ea1edb3ecdc0b1ccf008c0f3d0630
Damian Zaremba <damian@damianzaremba.co.uk>
2021-11-01 22:11:56 +0100

    Retain 3 days of dumps

--- prune_backups.sh
+++ prune_backups.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-echo "Removing backups older than 7 days"
-find "$HOME/mysql_backups" -mtime +7 -delete
+echo "Removing backups older than 3 days"
+find "$HOME/mysql_backups" -mtime +3 -delete
