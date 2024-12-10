358b2ff726196b36af64a760f67581d42b2bbfdc
Damian Zaremba <damian@damianzaremba.co.uk>
2024-12-10 17:15:07 +0000

    k8s - support staging + botng

--- k8s.py
+++ k8s.py
@@ -6,26 +6,38 @@ import argparse
 import json
 import subprocess
 import sys
+import os
 from typing import List, Tuple
 
+BASE_DIR = os.environ['HOME']
+TOOL_NAME = os.environ['USER'].split('tools.')[1]
 SUPPORTED_APPS = {
     "core": {
         "image": "docker-registry.tools.wmflabs.org/toolforge-bullseye-standalone:latest",
-        "cwd": "/data/project/cluebotng/apps/core/current",
+        "cwd": f"{BASE_DIR}/apps/core/current",
         "command": ["./cluebotng", "-l", "-m", "live_run"],
         "ports": [(3565, "TCP")],
         "limits": {"cpu": "0.1", "memory": "512Mi"},
     },
     "bot": {
-        "image": "docker-registry.tools.wmflabs.org/toolforge-php74-sssd-base:latest",
-        "cwd": "/data/project/cluebotng/apps/bot",
+        "image": "docker-registry.tools.wmflabs.org/toolforge-php82-sssd-base",
+        "cwd": f"{BASE_DIR}/apps/bot",
         "command": ["php", "-f", "cluebot-ng.php"],
         "limits": {"cpu": "0.5", "memory": "2048Mi"},
-        "livenessCommand": ["php", "-f", "/data/project/cluebotng/apps/bot/health_check.php"],
+        "livenessCommand": ["php", "-f", f"{BASE_DIR}/apps/bot/health_check.php"],
+    },
+    "botng": {
+        "image": "docker-registry.tools.wmflabs.org/toolforge-php82-sssd-base",
+        "cwd": f"{BASE_DIR}/apps/botng/current",
+        "env": {"BOTNG_CFG": f"{BASE_DIR}/.botng.yaml", "BOTNG_LOG": f"{BASE_DIR}/botng.log"},
+        "command": [f"{BASE_DIR}/apps/botng/current/botng", "--irc-relay", "--debug"],
+        "ports": [(8118, "TCP")],
+        "limits": {"cpu": "0.5", "memory": "1024Mi"},
+        "livenessCommand": ["php", "-f", f"{BASE_DIR}/apps/bot/health_check.php"],
     },
     "irc-relay": {
         "image": "docker-registry.tools.wmflabs.org/toolforge-python39-sssd-base:latest",
-        "cwd": "/data/project/cluebotng/apps/irc_relay",
+        "cwd": f"{BASE_DIR}/apps/irc_relay",
         "command": ["./relay.py"],
         "ports": [(3334, "UDP")],
         "limits": {"cpu": "0.1", "memory": "100Mi"},
@@ -33,13 +45,13 @@ SUPPORTED_APPS = {
 }
 
 
-def build_deployment():
+def build_deployment(use_bot_ng):
     deployment = {
         "apiVersion": "apps/v1",
         "kind": "Deployment",
         "metadata": {
             "name": "cbng",
-            "namespace": "tool-cluebotng",
+            "namespace": f"tool-{TOOL_NAME}"
         },
         "spec": {
             "selector": {"matchLabels": {"cluebot.toolsforge.org/role": "cbng",
@@ -55,7 +67,10 @@ def build_deployment():
                             "name": task_name,
                             "command": [*task["command"]],
                             "env": [
-                                {"name": "HOME", "value": "/data/project/cluebotng"}
+                                {"name": "HOME", "value": BASE_DIR}
+                            ] + [
+                                {"name": k, "value": v}
+                                for k, v in task.get("env", {}).items()
                             ],
                             "image": task["image"],
                             "imagePullPolicy": "Always",
@@ -78,6 +93,13 @@ def build_deployment():
                             }
                         } if "livenessCommand" in task else {})}
                         for task_name, task in SUPPORTED_APPS.items()
+                        if (
+                            task_name not in {'bot', 'botng', 'irc-relay'}
+                            or
+                            (task_name in {'bot', 'irc-relay'} and not use_bot_ng)
+                            or
+                            (task_name == 'botng' and use_bot_ng)
+                        )
                     ] + [
                         # This is an awful hack to deal with the lack of services
                         # We can likely just hard code this if we stick with this
@@ -90,14 +112,14 @@ def build_deployment():
                                 "--",
                                 (
                                     "python3 -m pip install --upgrade pymysql &&"
-                                    " /data/project/cluebotng/apps/utilities/update_node.py"
+                                    f" {BASE_DIR}/apps/utilities/update_node.py"
                                 ) + "".join([
                                     " --task-name {}".format(task_name)
                                     for task_name in SUPPORTED_APPS.keys()
                                 ]),
                             ],
                             "env": [
-                                {"name": "HOME", "value": "/data/project/cluebotng"}
+                                {"name": "HOME", "value": BASE_DIR}
                             ],
                             "image": "docker-registry.tools.wmflabs.org/toolforge-python39-sssd-base:latest",
                             "imagePullPolicy": "Always",
@@ -121,9 +143,39 @@ def build_deployment():
     return deployment
 
 
-def build_and_apply():
-    deployable = json.dumps(build_deployment(), indent=4)
-    apply_deployable(deployable)
+def build_service(use_bot_ng):
+    if not use_bot_ng:
+        return None
+    return {
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {
+            "name": "cbng-metrics",
+            "namespace": f"tool-{TOOL_NAME}",
+        },
+        "spec": {
+            "type": "ClusterIP",
+            "selector": {
+                "name": "cbng",
+            },
+            "ports": [
+                {
+                    "name": "metrics",
+                    "port": 8118,
+                    "protocol": "TCP",
+                    "targetPort": 8118,
+                }
+            ]
+        }
+    }
+
+
+def build_and_apply(use_bot_ng):
+    if deployable := build_deployment(use_bot_ng):
+        apply_deployable(json.dumps(deployable))
+
+    if service := build_service(use_bot_ng):
+        apply_deployable(json.dumps(service))
 
 
 def apply_deployable(deployment):
@@ -142,33 +194,42 @@ def apply_deployable(deployment):
     print(p.stdout.decode("utf-8"))
 
 
-def delete(soft_fail=False):
+def _delete(resource, name):
     p = subprocess.run(
-        ["kubectl", "delete", "deployment", "cbng"],
+        ["kubectl", "delete", resource, name],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
     if p.returncode != 0:
-        print("Error deleting:")
+        print(f"Error deleting {resource}/{name}:")
         print(p.stderr.decode("utf-8"))
-        if not soft_fail:
-            sys.exit(p.returncode)
-    else:
-        print("Successfully deleted:")
-        print(p.stdout.decode("utf-8"))
+        return False
+
+    print(f"Successfully deleted {resource}/{name}:")
+    print(p.stdout.decode("utf-8"))
+    return True
+
+
+def delete(soft_fail=False):
+    is_success = True
+    is_success &= _delete("deployment", "cbng")
+    is_success &= _delete("service", "cbng-metrics")
+    if not is_success and not soft_fail:
+        sys.exit(1)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--deploy", action="store_true")
     parser.add_argument("--delete", action="store_true")
+    parser.add_argument("--botng", action="store_true")
     args = parser.parse_args()
 
     if args.delete:
         delete(args.deploy)
 
     if args.deploy:
-        build_and_apply()
+        build_and_apply(args.botng)
 
 
 if __name__ == "__main__":
