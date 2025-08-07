f81cad66620340b398aa780dc88b63154f07bf83
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-07 15:23:40 +0000

    Add support for emitting `dologmsg`
    This creates a SAL entry (message in `#wikimedia-cloud` & entry on tool SAL page e.g. https://wikitech.wikimedia.org/wiki/Nova_Resource:Tools.cluebotng/SAL)

--- fabfile.py
+++ fabfile.py
@@ -27,7 +27,7 @@ def _build_composer_command(home_dir, working_dir, command):
                     "metadata": {
                         "labels": {
                             "toolforge": "tool",
-                            "toolforge.org/mount-storage": "all"
+                            "toolforge.org/mount-storage": "all",
                         }
                     },
                     "stdin": True,
@@ -36,24 +36,18 @@ def _build_composer_command(home_dir, working_dir, command):
                     "command": command,
                     "env": [{"name": "HOME", "value": home_dir.as_posix()}],
                     "volumeMounts": [
-                        {
-                            "mountPath": "/data/project",
-                            "name": "home"
-                        },
+                        {"mountPath": "/data/project", "name": "home"},
                     ],
-                    "workingDir": working_dir.as_posix()
+                    "workingDir": working_dir.as_posix(),
                 }
             ],
             "volumes": [
                 {
-                    "hostPath": {
-                        "path": "/data/project",
-                        "type": "Directory"
-                    },
-                    "name": "home"
+                    "hostPath": {"path": "/data/project", "type": "Directory"},
+                    "name": "home",
                 },
-            ]
-        }
+            ],
+        },
     }
 
     return (
@@ -67,38 +61,43 @@ def _build_composer_command(home_dir, working_dir, command):
     )
 
 
-UTILITIES_BRANCH = 'main'
-EXTERNAL_ALLOY_RELEASE = '1.10.0'
+UTILITIES_BRANCH = "main"
+EXTERNAL_ALLOY_RELEASE = "1.10.0"
+EMIT_LOG_MESSAGES = os.environ.get("EMIT_LOG_MESSAGES", "true") == "true"
 TARGET_RELEASE = os.environ.get("TARGET_RELEASE")
 TARGET_USER = os.environ.get("TARGET_USER", "cluebotng")
 PRODUCTION_USER = "cluebotng"
-TOOL_DIR = PosixPath('/data/project') / TARGET_USER
+TOOL_DIR = PosixPath("/data/project") / TARGET_USER
 
 c = Connection(
-    'login.toolforge.org',
+    "login.toolforge.org",
     config=Config(
         overrides={
-            'sudo': {
-                'user': f'tools.{os.environ.get("TARGET_USER", TARGET_USER)}',
-                'prefix': '/usr/bin/sudo -ni'
+            "sudo": {
+                "user": f'tools.{os.environ.get("TARGET_USER", TARGET_USER)}',
+                "prefix": "/usr/bin/sudo -ni",
             }
         }
     ),
 )
 
 
-def __get_file_contents(path: str, parent: str = 'static') -> str:
-    with (PosixPath(__file__).parent / parent / path).open('r') as fh:
+def __get_file_contents(path: str, parent: str = "static") -> str:
+    with (PosixPath(__file__).parent / parent / path).open("r") as fh:
         return fh.read()
 
 
-def __write_remote_file_contents(path: str, contents: str, overwrite: bool = True, replace_vars = None):
+def __write_remote_file_contents(
+    path: str, contents: str, overwrite: bool = True, replace_vars=None
+):
     replace_vars = {} if replace_vars is None else replace_vars
     for key, value in replace_vars.items():
         contents = contents.replace(f'{"{{"} {key} {"}}"}', value)
-    encoded_contents = base64.b64encode(contents.encode('utf-8')).decode('utf-8')
-    overwrite_check = f'test -f \'{path}\' || ' if not overwrite else ""
-    c.sudo(f'bash -c "umask 026 && ({overwrite_check}base64 -d > \'{path}\' <<< \'{encoded_contents}\')"')
+    encoded_contents = base64.b64encode(contents.encode("utf-8")).decode("utf-8")
+    overwrite_check = f"test -f '{path}' || " if not overwrite else ""
+    c.sudo(
+        f"bash -c \"umask 026 && ({overwrite_check}base64 -d > '{path}' <<< '{encoded_contents}')\""
+    )
 
 
 def _setup():
@@ -106,62 +105,74 @@ def _setup():
     c.sudo(f'mkdir -p {TOOL_DIR / "apps"}')
     c.sudo(f'mkdir -p {TOOL_DIR / "apps" / "core"}')
     c.sudo(f'mkdir -p {TOOL_DIR / "apps" / "core" / "releases"}')
-    c.sudo(f'bash -c \'test -d {TOOL_DIR / "apps" / "utilities"} || '
-           f'git clone https://github.com/cluebotng/utilities.git {TOOL_DIR / "apps" / "utilities"}\'')
+    c.sudo(
+        f'bash -c \'test -d {TOOL_DIR / "apps" / "utilities"} || '
+        f'git clone https://github.com/cluebotng/utilities.git {TOOL_DIR / "apps" / "utilities"}\''
+    )
 
 
 def _restart_jobs(targets=None):
     if targets is None:
         targets = []
-        if TARGET_USER == 'cluebotng-staging':
+        if TARGET_USER == "cluebotng-staging":
             targets.extend(["botng", "core", "grafana-alloy", "webservice"])
-        if TARGET_USER == 'cluebotng':
+        if TARGET_USER == "cluebotng":
             targets.extend(["bot", "core", "webservice"])
 
     for target in targets:
-        print(f'Restarting {target}')
-        if target == 'webservice':
-            c.sudo(f"XDG_CONFIG_HOME={TOOL_DIR} toolforge webservice buildservice restart")
+        print(f"Restarting {target}")
+        if target == "webservice":
+            c.sudo(
+                f"XDG_CONFIG_HOME={TOOL_DIR} toolforge webservice buildservice restart"
+            )
         else:
-            c.sudo(f'XDG_CONFIG_HOME={TOOL_DIR} toolforge jobs restart {target}')
+            c.sudo(f"XDG_CONFIG_HOME={TOOL_DIR} toolforge jobs restart {target}")
 
 
 def _update_utilities():
     """Update the utilities release."""
-    print(f'Updating utilities')
-    release_dir = TOOL_DIR / 'apps' / 'utilities'
+    print(f"Updating utilities")
+    release_dir = TOOL_DIR / "apps" / "utilities"
 
-    c.sudo(f'git -C {release_dir} reset --hard')
-    c.sudo(f'git -C {release_dir} clean -fd')
-    c.sudo(f'git -C {release_dir} fetch -a')
-    c.sudo(f'git -C {release_dir} checkout {UTILITIES_BRANCH}')
-    c.sudo(f'git -C {release_dir} pull origin {UTILITIES_BRANCH}')
+    c.sudo(f"git -C {release_dir} reset --hard")
+    c.sudo(f"git -C {release_dir} clean -fd")
+    c.sudo(f"git -C {release_dir} fetch -a")
+    c.sudo(f"git -C {release_dir} checkout {UTILITIES_BRANCH}")
+    c.sudo(f"git -C {release_dir} pull origin {UTILITIES_BRANCH}")
 
-    print('Updating lighttpd configuration')
+    print("Updating lighttpd configuration")
     c.sudo(f'cp -fv {release_dir / "lighttpd.conf"} {TOOL_DIR}/.lighttpd.conf')
 
 
 def _update_jobs():
     """Update the job config."""
-    print(f'Updating jobs')
-    database_user = c.sudo(
-        f"awk -F= '{'{'}if($1 == \"user\") print $2{'}'}' {TOOL_DIR / 'replica.my.cnf'}", hide="stdout"
-    ).stdout.strip().strip("'").strip('"')
+    print(f"Updating jobs")
+    database_user = (
+        c.sudo(
+            f"awk -F= '{'{'}if($1 == \"user\") print $2{'}'}' {TOOL_DIR / 'replica.my.cnf'}",
+            hide="stdout",
+        )
+        .stdout.strip()
+        .strip("'")
+        .strip('"')
+    )
 
-    __write_remote_file_contents(TOOL_DIR / "jobs.yaml",
-                                 __get_file_contents(f'{TARGET_USER}.yaml', parent='jobs'),
-                                 replace_vars={
-                                     'tool_dir': TOOL_DIR.as_posix(),
-                                     'database_user': database_user,
-                                 })
+    __write_remote_file_contents(
+        TOOL_DIR / "jobs.yaml",
+        __get_file_contents(f"{TARGET_USER}.yaml", parent="jobs"),
+        replace_vars={
+            "tool_dir": TOOL_DIR.as_posix(),
+            "database_user": database_user,
+        },
+    )
 
     c.sudo(f'XDG_CONFIG_HOME={TOOL_DIR} toolforge jobs load {TOOL_DIR / "jobs.yaml"}')
 
 
 def _update_bot():
     """Update the bot release."""
-    target_release = TARGET_RELEASE or _get_latest_github_release('cluebotng', 'bot')
-    print(f'Moving bot to {target_release}')
+    target_release = TARGET_RELEASE or _get_latest_github_release("cluebotng", "bot")
+    print(f"Moving bot to {target_release}")
 
     # Update the latest image to our target release
     c.sudo(
@@ -172,11 +183,13 @@ def _update_bot():
         "https://github.com/cluebotng/bot.git"
     )
 
+    return target_release
+
 
 def _update_report():
     """Update the report release."""
-    target_release = TARGET_RELEASE or _get_latest_github_release('cluebotng', 'report')
-    print(f'Moving report to {target_release}')
+    target_release = TARGET_RELEASE or _get_latest_github_release("cluebotng", "report")
+    print(f"Moving report to {target_release}")
 
     # Update the latest image to our target release
     c.sudo(
@@ -188,17 +201,20 @@ def _update_report():
     )
 
     # Ensure the service template exists
-    __write_remote_file_contents(TOOL_DIR / "service.template",
-                                 __get_file_contents('report/service.template'))
+    __write_remote_file_contents(
+        TOOL_DIR / "service.template", __get_file_contents("report/service.template")
+    )
+
+    return target_release
 
 
 def _update_irc_relay():
     """Update the IRC relay release."""
     if TARGET_USER != PRODUCTION_USER:
-        return
+        return None
 
-    target_release = _get_latest_github_release('cluebotng', 'irc_relay')
-    print(f'Moving irc-relay to {target_release}')
+    target_release = _get_latest_github_release("cluebotng", "irc_relay")
+    print(f"Moving irc-relay to {target_release}")
 
     # Update the latest image to our target release
     c.sudo(
@@ -209,37 +225,46 @@ def _update_irc_relay():
         "https://github.com/cluebotng/irc_relay.git"
     )
 
+    return target_release
+
 
 def _update_core():
     """Update the core release."""
-    CORE_RELEASE = _get_latest_github_release('cluebotng', 'core')
-    print(f'Moving core to {CORE_RELEASE}')
-    release_dir = TOOL_DIR / "apps" / "core" / "releases" / CORE_RELEASE
+    target_release = _get_latest_github_release("cluebotng", "core")
+    print(f"Moving core to {target_release}")
+    release_dir = TOOL_DIR / "apps" / "core" / "releases" / target_release
 
     # Bins
-    c.sudo(f'mkdir -p {release_dir}')
-    c.sudo(f'bash -c \'test -f {release_dir / "cluebotng"} || wget -nv -O {release_dir / "cluebotng"}'
-           f' https://github.com/cluebotng/core/releases/download/{CORE_RELEASE}/cluebotng\'')
+    c.sudo(f"mkdir -p {release_dir}")
+    c.sudo(
+        f'bash -c \'test -f {release_dir / "cluebotng"} || wget -nv -O {release_dir / "cluebotng"}'
+        f" https://github.com/cluebotng/core/releases/download/{target_release}/cluebotng'"
+    )
     c.sudo(f'chmod 755 {release_dir / "cluebotng"}')
 
     c.sudo(f'mkdir -p {release_dir / "data"}')
-    for obj in {'main_ann.fann', 'bayes.db', 'two_bayes.db'}:
-        c.sudo(f'bash -c \'test -f {release_dir / "data" / obj} || wget -nv -O {release_dir / "data" / obj}'
-               f' https://github.com/cluebotng/core/releases/download/{CORE_RELEASE}/{obj}\'')
+    for obj in {"main_ann.fann", "bayes.db", "two_bayes.db"}:
+        c.sudo(
+            f'bash -c \'test -f {release_dir / "data" / obj} || wget -nv -O {release_dir / "data" / obj}'
+            f" https://github.com/cluebotng/core/releases/download/{target_release}/{obj}'"
+        )
         c.sudo(f'chmod 640 {release_dir / "data" / obj}')
 
-    c.sudo(f'bash -c \'test -f {release_dir}/conf.tar.gz || wget -nv -O {release_dir}/conf.tar.gz'
-           f' https://github.com/cluebotng/core/releases/download/{CORE_RELEASE}/conf.tar.gz\'')
-    c.sudo(f'tar -C {release_dir} -xvf {release_dir}/conf.tar.gz')
-    c.sudo(f'rm -f {release_dir}/conf.tar.gz')
+    c.sudo(
+        f"bash -c 'test -f {release_dir}/conf.tar.gz || wget -nv -O {release_dir}/conf.tar.gz"
+        f" https://github.com/cluebotng/core/releases/download/{target_release}/conf.tar.gz'"
+    )
+    c.sudo(f"tar -C {release_dir} -xvf {release_dir}/conf.tar.gz")
+    c.sudo(f"rm -f {release_dir}/conf.tar.gz")
 
     c.sudo(f'ln -snf {release_dir} {TOOL_DIR / "apps" / "core" / "current"}')
+    return target_release
 
 
 def _update_bot_ng():
     """Update the bot-ng release."""
-    target_release = TARGET_RELEASE or _get_latest_github_release('cluebotng', 'botng')
-    print(f'Moving botng to {target_release}')
+    target_release = TARGET_RELEASE or _get_latest_github_release("cluebotng", "botng")
+    print(f"Moving botng to {target_release}")
 
     # Update the latest image to our target release
     c.sudo(
@@ -249,13 +274,14 @@ def _update_bot_ng():
         "-i botng "
         "https://github.com/cluebotng/botng.git"
     )
+    return target_release
 
 
 def _update_metrics_relay():
     """Update the grafana allow release."""
-    print(f'Moving grafana-alloy to {EXTERNAL_ALLOY_RELEASE}')
-    target_release = _get_latest_github_release('cluebotng', 'irc_relay')
-    print(f'Moving irc-relay to {target_release}')
+    print(f"Moving grafana-alloy to {EXTERNAL_ALLOY_RELEASE}")
+    target_release = _get_latest_github_release("cluebotng", "irc_relay")
+    print(f"Moving irc-relay to {target_release}")
 
     # Update the latest image to our target release
     c.sudo(
@@ -265,6 +291,11 @@ def _update_metrics_relay():
         "-i irc-relay "
         "https://github.com/cluebotng/irc_relay.git"
     )
+
+
+def _do_log_message(message: str):
+    """Emit a log message (from the tool account)."""
+    c.sudo(f"{'' if EMIT_LOG_MESSAGES else 'echo '}dologmsg '{message}'")
 
 
 @task()
@@ -277,27 +308,30 @@ def deploy_utilities(c):
 @task()
 def deploy_report(c):
     """Deploy the report interface to the current release."""
-    _update_report()
-    _restart_jobs(['webservice'])
+    target_release = _update_report()
+    _restart_jobs(["webservice"])
+    _do_log_message(f"report deployed @ {target_release}")
 
 
 @task()
 def deploy_bot(c):
     """Deploy bot to the current release."""
     if TARGET_USER == PRODUCTION_USER:
-        _update_bot()
-        _restart_jobs(['bot'])
+        target_release = _update_bot()
+        _restart_jobs(["bot"])
     else:
-        _update_bot_ng()
-        _restart_jobs(['botng'])
+        target_release = _update_bot_ng()
+        _restart_jobs(["botng"])
+    _do_log_message(f"bot deployed @ {target_release}")
 
 
 @task()
 def deploy_core(c):
     """Deploy the core to the current release."""
     _setup()
-    _update_core()
-    _restart_jobs(['core'])
+    target_release = _update_core()
+    _restart_jobs(["core"])
+    _do_log_message(f"core deployed @ {target_release}")
 
 
 @task()
@@ -305,15 +339,16 @@ def deploy_metrics_relay(c):
     """Deploy the metrics relay to the current release."""
     _update_metrics_relay()
     if TARGET_USER != PRODUCTION_USER:
-        _restart_jobs(['grafana-alloy'])
+        _restart_jobs(["grafana-alloy"])
 
 
 @task()
 def deploy_irc_relay(c):
     """Deploy the irc relay to the current release."""
     if TARGET_USER == PRODUCTION_USER:
-        _update_irc_relay()
-        _restart_jobs(['irc-relay'])
+        target_release = _update_irc_relay()
+        _restart_jobs(["irc-relay"])
+        _do_log_message(f"irc-relay deployed @ {target_release}")
 
 
 @task()
